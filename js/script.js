23c7095021d3820b63118af60dcf7c8a15fdd195
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-01-31 16:27:04 +0100

    completato il js della griglia

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 // GRIGLIA DA 1 A 100
+const gridElement = document.querySelector("#grid");
+
+for(let i = 1; i <= 100; i++){
+
+    const newElement = document.createElement("div");
+    newElement.className = "square";
+    newElement.innerHTML = i;
+
+    gridElement.append(newElement);
+    
+    if(i % 3 == 0) {
+        
+        newElement.className = "square fizz";
+        newElement.innerHTML = `Fizz`;
+
+    } else if (i % 5 == 0) {
+
+        newElement.className = "square buzz";
+        newElement.innerHTML = `Buzz`;
+    }
+
+    if(i % 15 == 0){
+        
+        newElement.className = "square fizzbuzz";
+        newElement.innerHTML = `Fizzbuzz`;
+    }
+    
+
+}
